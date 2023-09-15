7099465eab517e4f885bf294da792f0f5795a179
Zhiger Kalymov <kalymov.zhiger@gmail.com>
2023-09-15 12:12:30 +0600

    version corrections

--- script-2.js
+++ script-2.js
@@ -1,54 +1,45 @@
         var Telegram = {
             token: null,
             to: null,
-            topic: null,
             message: null,
             proxy: null,
             parse_mode: null,
-        
+
             escapeMarkup: function (str, mode) {
                 switch (mode) {
                     case 'markdown':
                         return str.replace(/([_*\[`])/g, '\\$&');
-        
+
                     case 'markdownv2':
                         return str.replace(/([_*\[\]()~`>#+\-=|{}.!])/g, '\\$&');
 
-                    case 'html':
-                        return str.replace(/<(\s|[^a-z\/])/g, '&lt;$1');
-        
                     default:
                         return str;
                 }
             },
-        
             sendMessage: function () {
                 var params = {
-                    chat_id: Telegram.to,
-                    text: Telegram.message,
-                    disable_web_page_preview: true,
-                    disable_notification: false
-                },
-                data,
-                response,
-                request = new CurlHttpRequest(),
-                url = 'https://api.telegram.org/bot' + Telegram.token + '/sendMessage';
-        
+                        chat_id: Telegram.to,
+                        text: Telegram.message,
+                        disable_web_page_preview: true,
+                        disable_notification: false
+                    },
+                    data,
+                    response,
+                    request = new CurlHttpRequest(),
+                    url = 'https://api.telegram.org/bot' + Telegram.token + '/sendMessage';
+
                 if (Telegram.parse_mode !== null) {
                     params['parse_mode'] = Telegram.parse_mode;
                 }
 
-                if (Telegram.topic !== null) {
-                    params['message_thread_id'] = Telegram.topic;
-                }
-        
                 if (Telegram.proxy) {
                     request.SetProxy(Telegram.proxy);
                 }
 
                 request.AddHeader('Content-Type: application/json');
                 data = JSON.stringify(params);
-        
+
                 // Remove replace() function if you want to see the exposed token in the log file.
                 Zabbix.Log(4, '[Telegram Webhook] URL: ' + url.replace(Telegram.token, '<TOKEN>'));
                 Zabbix.Log(4, '[Telegram Webhook] params: ' + data);
@@ -72,35 +63,35 @@
                 }
             }
         };
-        
+
         try {
             var params = JSON.parse(value);
-        
+
             if (typeof params.Token === 'undefined') {
                 throw 'Incorrect value is given for parameter "Token": parameter is missing';
             }
-        
+
             Telegram.token = params.Token;
-        
+
             if (params.HTTPProxy) {
                 Telegram.proxy = params.HTTPProxy;
-            } 
-        
+            }
+
             params.ParseMode = params.ParseMode.toLowerCase();
-            
+
             if (['markdown', 'html', 'markdownv2'].indexOf(params.ParseMode) !== -1) {
                 Telegram.parse_mode = params.ParseMode;
             }
-        
+
             Telegram.to = params.To;
             Telegram.message = params.Subject + '\n' + params.Message;
-        
-            if (['markdown', 'html', 'markdownv2'].indexOf(params.ParseMode) !== -1) {
+
+            if (['markdown', 'markdownv2'].indexOf(params.ParseMode) !== -1) {
                 Telegram.message = Telegram.escapeMarkup(Telegram.message, params.ParseMode);
             }
-        
+
             Telegram.sendMessage();
-        
+
             return 'OK';
         }
         catch (error) {
